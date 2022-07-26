0ae7213bdca8be9f9eeb0b73a9c28d1b0a9be9c1
Sanchit Shinde <105198839+sanchit787@users.noreply.github.com>
2022-07-26 22:48:51 +0530

    Add files via upload

---
+++ alpha.py
@@ -0,0 +1,86 @@
+from email.mime import audio
+import pyttsx3
+import wikipedia
+import pywhatkit
+import os
+import sys
+import webbrowser
+import speech_recognition as sr
+import datetime
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour <=12:
+        speak("Good Morning!")
+    
+    elif hour>12 and hour<=18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("Hi boss It's Alpha. How can i help you")
+
+
+def  takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said : {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishme()
+
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'play' in query:
+            song = query.replace("play","")
+            speak("Playing" + song)
+            pywhatkit.playonyt(song)
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Boss, the time is {strTime}")
+
+        elif 'open studio' in query:
+            vspath = "C:\\Users\\User\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(vspath)
+
+        elif "alpha sleep" in query:
+            sys.exit()
